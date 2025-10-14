5c7d0ef61da5d03f8023fd6fba900c7932c3e03e
Aarya Gharat <aaryaagharat@gmail.com>
2025-10-14 23:55:33 +0530

    Add user registration (login form) with DB insert

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
 import os
 import psycopg2
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 DATABASE_URL = os.environ['DATABASE_URL']
 
 @app.route('/')
-def hello():
-    return "Hello World from Flask on Railway!"
+def home():
+    return redirect(url_for('login'))
 
-@app.route('/add', methods=['POST'])
-def add():
-    data = request.json
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    cur.execute("INSERT INTO mytable (info) VALUES (%s)", (data['info'],))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return jsonify({"status": "success"})
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-@app.route('/get', methods=['GET'])
-def get():
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    cur.execute("SELECT info FROM mytable")
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    return jsonify(rows)
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return "Registration Successful!"
+    return '''
+        <form method="post">
+            Username: <input name="username" type="text" required><br>
+            Password: <input name="password" type="password" required><br>
+            <input type="submit" value="Register">
+        </form>
+    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
